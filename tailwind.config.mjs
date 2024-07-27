5bdb9660ae75e768f5a1dde7f9d7f2baed9e1a4d
jimiryquai <james@jamesryan.dev>
2024-07-27 09:14:42 +0100

    ready to submit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
-	plugins: [],
+		extend: {
+      boxShadow: {
+      'custom': '0px 1px 2px -1px rgba(0, 0, 0, 0.10), 0px 1px 3px 0 rgba(0, 0, 0, 0.10)'
+      },
+    },
+  },
+	plugins: [
+   
+  ],
 }
